1c79833850ca3ee46b4964264326bc8a18151d1d
meruneru <meruneru1032@gmail.com>
2021-09-29 22:20:01 +0900

    enable gdb. Refar to https://www.cyamax.com/entry/2018/02/09/070000

--- onLin.sh
+++ onLin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CURRENT=$(cd $(dirname $0); pwd)
-docker run --rm -it -v $CURRENT:/home/myc -w /home/myc debian $@
+docker run --rm -it  --cap-add=SYS_PTRACE --security-opt="seccomp=unconfined" -v $CURRENT:/home/myc -w /home/myc compilerbook $@
